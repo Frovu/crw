d507496fade940f4458f77881c76484d373d064a
Frovu <sfrovis@gmail.com>
2022-12-04 20:37:08 +0300

    fix: timestamp column

--- server/scripts/init_database.py
+++ server/scripts/init_database.py
@@ -3,7 +3,8 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../cor
 from database import pg_conn, list_columns, get_column_type, TABLE_NAME
 
 if __name__ == '__main__':
-	columns = ',\n\t'.join([c+' '+get_column_type(c) for c in list_columns()])
+	rtype = lambda t: 'timestamp with time zone' if t == 'time' else t
+	columns = ',\n\t'.join([c+' '+rtype(get_column_type(c)) for c in list_columns()])
 	with pg_conn.cursor() as cursor:
 		text = f'''CREATE TABLE IF NOT EXISTS {TABLE_NAME} (
 	id serial primary key,
