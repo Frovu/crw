873a549d92c63865a1efd8fba638659a60c30d27
Frovu <sfrovis@gmail.com>
2022-12-06 22:50:24 +0300

    add: logging

--- server/server.py
+++ server/server.py
@@ -1,3 +1,38 @@
+import gzip
+import os
+import logging
+import logging.handlers
+
+if not os.path.exists('logs'):
+    os.makedirs('logs')
+def rotator(source, dest):
+    with open(source, 'rb') as sf:
+        data = sf.read()
+        compressed = gzip.compress(data)
+        with open(dest+'.gz', 'wb') as df:
+            df.write(compressed)
+    os.remove(source)
+
+formatter = logging.Formatter('%(asctime)s/%(levelname)s: %(message)s')
+log_rotate = logging.handlers.TimedRotatingFileHandler('logs/aid.log', 'midnight')
+log_rotate.rotator = rotator
+log_rotate.setLevel(logging.DEBUG)
+log_rotate.setFormatter(formatter)
+
+sh = logging.StreamHandler()
+sh.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.handlers = [ log_rotate, sh ]
+logger.setLevel(logging.DEBUG)
+
+# logging.getLogger('werkzeug').setLevel(logging.WARNING)
+# import requests
+# logging.getLogger('urllib3').setLevel(logging.WARNING)
+# logging.getLogger('urllib3').propagate = False
+
+logging.critical('STARTING SERVER')
+
 from flask import Flask, session
 from routers import admin
 from routers import forbush
