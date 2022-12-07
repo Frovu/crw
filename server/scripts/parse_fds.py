43a56ecc00d9e322e2fd9eb0ed70ef95a255df4b
Frovu <sfrovis@gmail.com>
2022-12-07 16:29:28 +0300

    add: parse column update

--- server/scripts/parse_fds.py
+++ server/scripts/parse_fds.py
@@ -2,6 +2,7 @@ from datetime import datetime, timezone
 import sys, os
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../core'))
 from database import pg_conn, tables_info
+import psycopg2.extras
 
 def _parse_value(split, columns, col, description):
 	value = split[columns.index(col)]
@@ -24,6 +25,52 @@ def _parse_value(split, columns, col, description):
 	except:
 		return None
 
+# only for updating columns in existing rows
+def parse_one_column(fname: str, table: str, column: str):
+	with open(fname) as file, pg_conn.cursor() as cursor:
+		for line in file:
+			if not 'Date' in line: continue
+			columns_order = line.strip().split()
+			break
+		first_table = list(tables_info)[0]
+		def recursive_search(tbl, target, path):
+			if tbl == target:
+				return path
+			for col_name, col_desc in tables_info[tbl].items():
+				if col_name.startswith('_'):
+					continue
+				if ref := col_desc.get('references'):
+					npath = f'(SELECT {col_name} FROM {tbl} WHERE id = {path})'
+					found = recursive_search(ref, target, npath)
+					if found: return found
+		select_id = recursive_search(first_table, table, f'(SELECT id FROM {first_table} WHERE time = data.time)')
+		data = []
+		time_col_desc = tables_info[first_table]['time']
+		time_col_name = time_col_desc['parse_name']
+		target_col_desc = tables_info[table][column]
+		target_col_name = target_col_desc['parse_name']
+		for line in file:
+			line_split = line.split()
+			time = _parse_value(line_split, columns_order, time_col_name, time_col_desc)
+			value = _parse_value(line_split, columns_order, target_col_name, target_col_desc)
+			data.append((time, value))
+		diff_q = f'SELECT data.time, {column}, data.val FROM (VALUES %s) AS data(time, val) INNER JOIN {table} ON id = {select_id} WHERE {column} != data.val'
+		psycopg2.extras.execute_values(cursor, diff_q, data, template='(%s, %s'+('::real' if target_col_desc.get('type', 'real') == 'real' else '')+')')
+		diff = cursor.fetchall()
+		if not len(diff):
+			print('already up to date')
+			return False
+		query = f'UPDATE {table} SET {column} = data.val FROM (VALUES %s) AS data(time, val) WHERE id = {select_id}'
+		print('\n' + query)
+		if len(diff) > 30:
+			print(f'...\n[{len(diff)-30}]\n...')
+		for d in diff[-30:]:
+			t, p, n = d
+			print(t, p, '->', n)
+		print(f'\nabout to change {len(diff)} rows')
+		psycopg2.extras.execute_values(cursor, query, data)
+		return True
+		
 # if target_columns contains field with Time, its corresponding Date column is parsed automatically
 def parse_whole_file(fname: str):
 	with open(fname) as file, pg_conn.cursor() as cursor:
@@ -64,14 +111,42 @@ def parse_whole_file(fname: str):
 		for table, cnt in count.items():
 			print(f'[{cnt}] -> {table}')
 
+def main():
+	FNAME = 'data/FDs_fulltable.txt'
+	if len(sys.argv) < 2:
+		if input(f'parse whole FDs file? [y/n]: ') != 'y':
+			return
+		parse_whole_file(FNAME)
+		if input(f'commit insertion? [y/n]: ') == 'y':
+			pg_conn.commit()
+		return
+
+	name_part = sys.argv[1].lower()
+	candidates = list()
+	for table, table_info in tables_info.items():
+		for col_name, col_desc in table_info.items():
+			if col_name.startswith('_'): continue
+			if not col_desc.get('parse_name'): continue
+			if name_part in col_name or name_part in col_desc.get('name', '').lower() or name_part in col_desc.get('parse_name', '').lower():
+				candidates.append((table, col_name))
+	if len(candidates) < 1:
+		return print(f'column not found: {name_part}')
+	elif len(candidates) == 1:
+		res = parse_one_column(FNAME, *candidates[0])
+	else:
+		print(f'found {len(candidates)} candidates:')
+		print('\n'.join([f'  {i}. {t}.{c}' for i, (t, c) in enumerate(candidates)]))
+		choice = input(f'please input which column to parse [0-{len(candidates)-1}]: ')
+		assert int(choice) >= 0 and int(choice) < len(candidates)
+		res = parse_one_column(FNAME, *candidates[int(choice)])
+	if res and input(f'commit updates? [y/n]: ') == 'y':
+		pg_conn.commit()
+
+
 # (A|B|C|M|X) ([\d\.]+) replace $1$2 
 # (\d)-\d9\d+ replace $1 -9900
 if __name__ == '__main__':
-	parse_whole_file('data/FDs_fulltable.txt')
-	print(f'commit? [y/n]')
-	if input() == 'y':
-		pg_conn.commit()
-
+	main()
 
 '''
 DELETE FROM forbush_effects;
