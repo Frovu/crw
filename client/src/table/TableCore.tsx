efe9fd1f672ec88597e18b683ab64a1bad7c8f92
Frovu <sfrovis@gmail.com>
2022-12-12 18:44:48 +0300

    add: keyboard navigation

--- client/src/table/TableCore.tsx
+++ client/src/table/TableCore.tsx
@@ -2,17 +2,23 @@ import { useState, useRef, useEffect, useContext, useLayoutEffect } from 'react'
 import { useEventListener } from '../util';
 import { ColumnDef, Sort, Cursor, prettyName, DataContext } from './Table';
 
-function Cell({ value, editing, def }: { value: any, editing: boolean, def: ColumnDef }) {
+function Cell({ value, cursor, def }: { value: any, cursor: Cursor, def: ColumnDef }) {
 	const val = value instanceof Date ? value.toISOString().replace(/\..+/, '').replace('T', ' ') : value;
-	if (!editing)
-		return <span className='Cell' style={{ width: def.width+.5+'ch' }}>{val}</span>;
-	return <input type='text' value={val}></input>;
+	const width = { width: def.width+.5+'ch' };
+	return (
+		<td style={{ ...(cursor && { borderColor: 'var(--color-active)' }) }}>
+			{!cursor?.editing &&
+			<span className='Cell' style={{ ...width }}>{val}</span>}
+			{cursor?.editing &&
+			<input style={{ ...width, border: 'none', padding: 0, boxShadow: ' 0 0 16px 4px var(--color-active)' }} autoFocus type='text' value={val}></input>}
+		</td>
+	);
 }
 
 function Row({ row, columns, cursor }: { row: any[], columns: ColumnDef[], cursor?: Cursor }) {
 
 	// eslint-disable-next-line react/no-array-index-key
-	return <tr>{row.map((value, i) =><td key={i}><Cell {...{ value, editing: cursor?.column === i, def: columns[i] }}/></td>)}</tr>;
+	return <tr>{row.map((value, i) =><Cell key={i} {...{ value, cursor: cursor?.column === i ? cursor : null, def: columns[i] }}/>)}</tr>;
 }
 
 function ColumnHeader({ col, sort, setSort }: { col: ColumnDef, sort: Sort, setSort: (s: Sort) => void}) {
@@ -43,7 +49,38 @@ export default function TableView({ sort, setSort, cursor, setCursor }: { sort: 
 	}, [data.length, ref]);
 
 	useEventListener('keydown', (e: KeyboardEvent) => {
-		console.log(e.key)
+		if (cursor?.editing) return;
+		if (cursor && e.key === 'Enter')
+			return setCursor({ ...cursor, editing: true });
+
+		const set = (curs: Exclude<Cursor, null>) => {
+			const newIdx = curs.row - 1 <= viewIndex ? curs.row - 1 : 
+				(curs.row + 1 >= viewIndex+viewSize ? curs.row - viewSize + 2 : viewIndex);
+			setViewIndex(Math.min(Math.max(newIdx, 0), data.length <= viewSize ? 0 : data.length - viewSize));
+			setCursor(curs);
+		};
+		if (e.ctrlKey && e.key === 'Home')
+			return set({ row: 0, column: cursor?.column ?? 0 });
+		if (e.ctrlKey && e.key === 'End')
+			return set({ row: data.length - 1, column: cursor?.column ?? 0 });
+
+		const delta = {
+			'ArrowUp': [-1, 0],
+			'ArrowDown': [1, 0],
+			'ArrowLeft': [0, -1],
+			'ArrowRight': [0, 1],
+			'PageUp': [-viewSize, 0],
+			'PageDown': [viewSize, 0],
+			'Home': [0, -columns.length],
+			'End': [0, columns.length]
+		}[e.key];
+		if (!delta) return;
+		const { row, column } = cursor ?? { row: Math.min(Math.round(viewIndex+viewSize/2), data.length), column: Math.round(columns.length/2) };
+		const [deltaRow, deltaCol] = delta;
+		set({
+			row: Math.min(Math.max(0, row + deltaRow), data.length - 1),
+			column: Math.min(Math.max(0, column + deltaCol), columns.length - 1)
+		});		
 	});
 
 	const tables = new Map<any, ColumnDef[]>();
