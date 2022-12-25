8c293060df1104226649d54a2fc7a5807b93bfc2
Frovu <sfrovis@gmail.com>
2022-12-25 15:52:17 +0300

    data: fetch omniweb

--- server/data_series/omni/database.py
+++ server/data_series/omni/database.py
@@ -2,6 +2,7 @@ import os, json, logging, requests
 import psycopg2, psycopg2.extras
 from datetime import datetime, timedelta, timezone
 from core.database import pg_conn
+from data_series.util import integrity_query
 
 log = logging.getLogger('aides')
 omniweb_url = 'https://omniweb.gsfc.nasa.gov/cgi/nx1.cgi'
@@ -40,8 +41,9 @@ def _init():
 _init()
 
 
-def _obtain_omniweb(dt_from: datetime, dt_to: datetime):
-	dstart, dend = [d.strftime('%Y%m%d') for d in [dt_from, dt_to]]
+def _obtain_omniweb(t_from: int, t_to: int):
+	dstart, dend = [datetime.utcfromtimestamp(t).strftime('%Y%m%d') for t in [t_from, t_to]]
+	log.debug(f'Omniweb: querying {dstart}:{dend}')
 	r = requests.post(omniweb_url, stream=True, data = {
 		'activity': 'retrieve',
 		'res': 'hour',
@@ -51,7 +53,7 @@ def _obtain_omniweb(dt_from: datetime, dt_to: datetime):
 		'vars': [c.omniweb_id for c in omni_columns]
 	})
 	if r.status_code != 200:
-		log.warn('Omni: query failed - HTTP {r.status_code}')
+		log.warn('Omniweb: query failed - HTTP {r.status_code}')
 
 	data = None
 	for line in r.iter_lines(decode_unicode=True):
@@ -61,10 +63,10 @@ def _obtain_omniweb(dt_from: datetime, dt_to: datetime):
 			try:
 				split = line.split()
 				time = datetime(int(split[0]), 1, 1, tzinfo=timezone.utc) + timedelta(days=int(split[1])-1, hours=int(split[2]))
-				row = [time] + [int(v) if c.is_int else float(v) for v, c in zip(split[3:], omni_columns)]
+				row = [time] + [(int(v) if c.is_int else float(v)) if v != c.stub_value else None for v, c in zip(split[3:], omni_columns)]
 				data.append(row)
 			except:
-				log.error('Omni: failed to parse line:\n' + line)
+				log.error('Omniweb: failed to parse line:\n' + line)
 		elif 'YEAR DOY HR' in line:
 			data = [] # start reading data
 
@@ -73,5 +75,17 @@ def _obtain_omniweb(dt_from: datetime, dt_to: datetime):
 	with pg_conn.cursor() as cursor:
 		psycopg2.extras.execute_values(cursor, query, data)
 	pg_conn.commit()
-	log.debug(f'Omni: upserting {len(data)} rows {dstart}:{dend}')
-	
+	log.debug(f'Omniweb: upserting {len(data)} rows {dstart}:{dend}')
+
+def fetch(interval: [int, int]):
+	columns = [c.name for c in omni_columns]
+	with pg_conn.cursor() as cursor:
+		cursor.execute(integrity_query(*interval, PERIOD, 'omni', columns))
+		if gaps := cursor.fetchall():
+			for gap in gaps:
+				try:
+					_obtain_omniweb(*gap)
+				except Exception as e:
+					log.error(f'Omniweb: failed to obtain {gap[0]}:{gap[1]}: {str(e)}')
+		cursor.execute(f'SELECT {",".join(columns)} FROM omni WHERE to_timestamp(%s) <= time AND time < to_timestamp(%s)', interval)
+		return cursor.fetchall(), [desc[0] for desc in cursor.description]
