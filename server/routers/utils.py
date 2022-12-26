3b183cdeee711b26513bb36f94c525133af62f3a
Frovu <sfrovis@gmail.com>
2022-12-26 21:12:51 +0300

    routes: logger

--- server/routers/utils.py
+++ server/routers/utils.py
@@ -1,14 +1,15 @@
 import logging, traceback
+log = logging.getLogger('aides')
 
 def route_shielded(func):
     def wrapper(*args, **kwargs):
         try:
             return func(*args, **kwargs)
         except ValueError as e:
-            if str(e): logging.error(f'Error in {func.__name__}: {traceback.format_exc()}')
+            if str(e): log.error(f'Error in {func.__name__}: {traceback.format_exc()}')
             return {}, 400
         except Exception:
-            logging.error(f'Error in {func.__name__}: {traceback.format_exc()}')
+            log.error(f'Error in {func.__name__}: {traceback.format_exc()}')
             return {}, 500
     wrapper.__name__ = func.__name__
     return wrapper
