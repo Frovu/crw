a04bc0601a58e6fd34a16aea13af5601e45e72c5
Frovu <sfrovis@gmail.com>
2023-01-11 15:42:58 +0300

    data: compute temp idx

--- server/data_series/omni/database.py
+++ server/data_series/omni/database.py
@@ -4,12 +4,15 @@ from concurrent.futures import ThreadPoolExecutor
 from datetime import datetime, timedelta, timezone
 from core.database import pg_conn
 from data_series.util import integrity_query
+from data_series.omni.derived import compute_derived
 
 log = logging.getLogger('aides')
 omniweb_url = 'https://omniweb.gsfc.nasa.gov/cgi/nx1.cgi'
 PERIOD = 3600
 
 omni_columns = None
+column_names = None
+
 obtains_cache = dict()
 
 class OmniColumn:
@@ -20,7 +23,7 @@ class OmniColumn:
 		self.is_int = is_int
 
 def _init():
-	global omni_columns
+	global omni_columns, column_names
 	json_path = os.path.join(os.path.dirname(__file__), './database.json')
 	vard_path = os.path.join(os.path.dirname(__file__), './omni_variables.txt')
 	with open(json_path) as file, pg_conn.cursor() as cursor:
@@ -40,6 +43,7 @@ def _init():
 					continue
 				# Note: omniweb variables descriptions ids start with 1 but internally they start with 0, hence -1
 				omni_columns.append(OmniColumn(column, owid - 1, spl[2], 'int' in typedef.lower()))
+	column_names = [c.name for c in omni_columns] + [col for col, [td, owid] in columns.items() if owid is None and col != 'time']
 _init()
 
 
@@ -80,8 +84,9 @@ def _obtain_omniweb(dt_from: datetime, dt_to: datetime):
 			log.info(f'Omniweb: correcting range to fit {correct_range[0]}:{correct_range[1]}')
 			return _obtain_omniweb(max(new_range[0], dt_from), min(new_range[1], dt_to))
 
-	query = f'''INSERT INTO omni (time, {",".join([c.name for c in omni_columns])}) VALUES %s
-		ON CONFLICT (time) DO UPDATE SET {",".join([f"{c.name} = EXCLUDED.{c.name}" for c in omni_columns])}'''
+	data = compute_derived(data, [c.name for c in omni_columns])
+	query = f'''INSERT INTO omni (time, {",".join(column_names)}) VALUES %s
+		ON CONFLICT (time) DO UPDATE SET {",".join([f"{c} = EXCLUDED.{c}" for c in column_names])}'''
 	with pg_conn.cursor() as cursor:
 		psycopg2.extras.execute_values(cursor, query, data)
 	pg_conn.commit()
@@ -95,17 +100,17 @@ def _obtain(gap):
 	obtains_cache.pop(gap, None)
 
 def fetch(interval: [int, int], query, refetch=False, epoch=True):
-	columns = [c.name for c in omni_columns if not query or c.name in query]
+	columns = [c for c in column_names if c in query] if query else column_names
 	if len(columns) < 1:
 		raise ValueError('Zero fields match query')
 	with pg_conn.cursor() as cursor:
 		cursor.execute(integrity_query(interval, PERIOD, 'omni', columns if refetch else ['time'], return_epoch=False))
 		if gaps := cursor.fetchall():
 			for gap in gaps:
-				try:
-					_obtain(gap)
-				except Exception as e:
-					log.error(f'Omni: failed to obtain {gap[0]} to {gap[1]}: {str(e)}')
+				# try:
+				_obtain(gap)
+				# except Exception as e:
+				# 	log.error(f'Omni: failed to obtain {gap[0]} to {gap[1]}: {str(e)}')
 		cursor.execute(f'SELECT {"EXTRACT(EPOCH FROM time)::integer as" if epoch else ""} time, {",".join(columns)} ' +
 			'FROM omni WHERE to_timestamp(%s) <= time AND time < to_timestamp(%s) ORDER BY time', interval)
 		return cursor.fetchall(), [desc[0] for desc in cursor.description]
