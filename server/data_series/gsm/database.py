5d27ee7bbc5ad58d34534e73a4757e8365e40c9a
Frovu <sfrovis@gmail.com>
2023-03-31 00:19:42 +0300

    gsm send everything

--- server/data_series/gsm/database.py
+++ server/data_series/gsm/database.py
@@ -9,7 +9,7 @@ with pool.connection() as conn:
 	A10 REAL, A10m REAL, Ax REAL, Ay REAL, Az REAL, Axy REAL)''')
 
 def select(interval: [int, int], what=None):
-	if not what: what = 'A10m,Ax,Ay,Az,Axy'
+	if not what: what = 'A10m,A10,Ax,Ay,Az,Axy'
 	with pool.connection() as conn:
 		q = f'SELECT EXTRACT(EPOCH FROM time)::integer as time, {what} FROM gsm_result WHERE time >= to_timestamp(%s) AND time <= to_timestamp(%s) ORDER BY time'
 		curs = conn.execute(q, interval)
