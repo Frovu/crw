c8dbdf80fa00cd2fe16ce79120bcba2658ed42cb
Frovu <sfrovis@gmail.com>
2023-06-25 19:12:31 +0300

    fix local neutron file not found

--- server/data_series/neutron/database.py
+++ server/data_series/neutron/database.py
@@ -80,7 +80,7 @@ def _obtain_local(interval, stations):
 	dt_from, dt_to = [datetime.utcfromtimestamp(t) for t in interval]
 	dirp = Path(os.environ.get('NM_C_PATH')).resolve()
 	if not dirp.is_dir():
-		return logging.error('Dir not found: ' + str(dirp))
+		return log.error('Dir not found: ' + str(dirp))
 	data = dict()
 	for station_i, station in enumerate(stations):
 		def add_count(date, count):
@@ -88,21 +88,21 @@ def _obtain_local(interval, stations):
 				data[date] = [None for s in stations]
 			data[date][station_i] = count
 		for year in range(dt_from.year, dt_to.year + 1):
-			p = next(dirp.glob(str(year) + '[cC]'), None)
-			if not p: continue
-			p = next((d for d in p.iterdir() if d.name.upper().endswith(station)), None)
-			if not p:
-				logging.debug(f'Neutron: Not found NM station: {year}/{station}')
+			path = next(dirp.glob(str(year) + '[cC]'), None)
+			if not path: continue
+			path = next((d for d in path.iterdir() if d.name.upper().endswith(station)), None)
+			if not path:
+				log.debug(f'Neutron: Not found NM station: {year}/{station}')
 				continue
 			for month in range(1 if year != dt_from.year else dt_from.month, 13 if year != dt_to.year else dt_to.month + 1):
-				files = p.glob(f'{year%100:02}{month:02}*')
-				p = next((f for f in files if f.suffix and f.suffixes[0].upper() in ['.C0C', '.60C']), None)
-				if not p:
-					logging.debug(f'Neutron: Not found NM counts file: {year}/{station}/{month}')
+				files = path.glob(f'{year%100:02}{month:02}*')
+				file_path = next((f for f in files if f.suffix and f.suffixes[0].upper() in ['.C0C', '.60C']), None)
+				if not file_path:
+					log.info(f'Neutron: Not found NM counts file: {year}/{station}/{month}')
 					continue
 				try:
-					with open(p) as file:
-						if '.C0C' in p.name.upper():
+					with open(file_path) as file:
+						if '.C0C' in file_path.name.upper():
 							for i in range(7):
 								next(file) # skip comment
 							time_cursor = datetime(year, month, 1)
@@ -120,7 +120,7 @@ def _obtain_local(interval, stations):
 								cnt = float(line[24:].strip()) * 60 # Hz => imp/min
 								add_count(date, cnt)
 				except Exception as e:
-					logging.warn(f'Failed to parse {p}: {e}')
+					log.warn(f'Failed to parse {file_path}: {e}')
 
 	data = [[date, *data[date]] for date in data]
 	log.debug(f'Neutron: obtained local [{len(data)} * {len(stations)}] {dt_from} to {dt_to}')
