d8f269e922f8ce6af37602c23a7dc31a873a132b
Frovu <sfrovis@gmail.com>
2023-08-31 19:51:51 +0300

    plot export list ui

--- client/src/table/ExportPlot.tsx
+++ client/src/table/ExportPlot.tsx
@@ -22,7 +22,9 @@ const plotsList = Object.keys(trivialPlots) as (keyof typeof trivialPlots)[];
 
 type PlotSettings = {
 	type: keyof typeof trivialPlots,
-	height: number
+	height: number,
+	showTime: boolean,
+	id: number
 };
 type PlotExportSettings = {
 	width: number,
@@ -33,15 +35,23 @@ const defaultSettings = (): PlotExportSettings => ({
 	plots: [{
 		type: 'Solar Wind',
 		height: 200,
+		showTime: true,
+		id: 0,
 	}, {
 		type: 'Cosmic Rays',
 		height: 200,
+		showTime: true,
+		id: 1,
 	}, {
 		type: 'Geomagn',
 		height: 100,
+		showTime: false,
+		id: 2,
 	}, {
 		type: 'Ring of Stations',
 		height: 300,
+		showTime: true,
+		id: 3,
 	}]
 });
 
@@ -51,7 +61,8 @@ export default function PlotExportView({ escape }: { escape: () => void }) {
 	const [settings, setSettings] = usePersistedState('aidPlotExport', defaultSettings);
 
 	const set = <T extends keyof PlotExportSettings>(what: T, val: PlotExportSettings[T]) => setSettings(st => ({ ...st, [what]: val }));
-	const setPlot = <T extends keyof PlotSettings>(i: number, k: T, v: PlotSettings[T]) => setSettings(st => {
+	const setPlot = <T extends keyof PlotSettings>(id: number, k: T, v: PlotSettings[T]) => setSettings(st => {
+		const i = st.plots.findIndex(p => p.id === id);
 		st.plots[i] = { ...st.plots[i], [k]: v };
 		return { ...st, plots: st.plots };
 	});
@@ -89,25 +100,37 @@ export default function PlotExportView({ escape }: { escape: () => void }) {
 		a.click();
 	};
 
+	const clamp = (min: number, max: number, val: number) => Math.max(min, Math.min(max, val));
 	return (<div style={{ display: 'flex', userSelect: 'none', width: settings.width + 320 + 8, padding: 16 }}>
 		<div style={{ width: 320 }} ref={el => el?.addEventListener('wheel', e => e.preventDefault(), { passive: false })}>
 			<div>
-				<label>Width: <input style={{ width: 64 }} type='number' min='200' max='7200' step='20' onWheel={e => e.nativeEvent.stopPropagation()}
-					value={settings.width} onChange={e => set('width', Math.max(320, Math.min(7200, e.target.valueAsNumber)))}/> px</label>
+				<label>Width: <input style={{ width: 64 }} type='number' min='200' max='3600' step='20'
+					onWheel={(e: any) => set('width', clamp(320, 3600, (e.target.valueAsNumber || 0) + (e.deltaY < 0 ? 20 : -20)))}
+					value={settings.width} onChange={e => set('width', clamp(320, 3600, e.target.valueAsNumber))}/> px</label>
 				
 			</div>
 			<div style={{ padding: 8 }}>
-				{settings.plots.map((plot, i) => <div style={{ marginTop: 8 }} key={JSON.stringify(plot)}>
-					<select style={{ width: 128 }} value={plot.type} onChange={e => setPlot(i, 'type', e.target.value as any)}
-						onWheel={e => setPlot(i, 'type',
-							plotsList[(plotsList.indexOf(plot.type) + (e.deltaY > 0 ? 1 : -1) + plotsList.length) % plotsList.length] as any)}>
+				{settings.plots.map(({ type, height, id, showTime }) => <div style={{ marginTop: 8, position: 'relative' }} key={id}>
+					<select style={{ width: 128 }} value={type} onChange={e => setPlot(id, 'type', e.target.value as any)}
+						onWheel={e => setPlot(id, 'type',
+							plotsList[(plotsList.indexOf(type) + (e.deltaY < 0 ? 1 : -1) + plotsList.length) % plotsList.length] as any)}>
 						{plotsList.map(ptype =>
 							<option key={ptype} value={ptype}>{ptype}</option>)}
 					</select>
-					<label title='Plot height'> h=<input style={{ width: 54 }} type='number' min='80' max='2000' step='20'
-						value={plot.height} onChange={e => setPlot(i, 'height', e.target.valueAsNumber)}></input></label>
+					<label title='Plot height'> h=<input style={{ width: 54 }} type='number' min='80' max='1800' step='20'
+						onWheel={(e: any) => setPlot(id, 'height', clamp(80, 1800, (e.target.valueAsNumber || 0) + (e.deltaY < 0 ? 20 : -20)))}
+						value={height} onChange={e => setPlot(id, 'height', clamp(80, 1800, e.target.valueAsNumber))}></input></label>
+					<label style={{ cursor: 'pointer' }}> tm<input type='checkbox' checked={showTime} onChange={e => setPlot(id, 'showTime', e.target.checked)}/></label>
+					<span style={{ position: 'absolute', right: 0, top: 1 }} className='CloseButton' onClick={() =>
+						set('plots', settings.plots.filter(p => p.id !== id))}>&times;</span>
 				</div> )}
-				<button style={{ marginTop: 4, borderColor: 'transparent', color: color('skyblue'), cursor: 'pointer' }}>+ <u>add new plot</u></button>
+				<button style={{ marginTop: 4, borderColor: 'transparent', color: color('skyblue'), cursor: 'pointer' }}
+					onClick={() => set('plots', settings.plots.concat({
+						height: 200,
+						type: plotsList.find(t => !settings.plots.find(p => p.type === t)) ?? plotsList[0],
+						showTime: true,
+						id: Date.now()
+					}))}>+ <u>add new plot</u></button>
 			</div>
 			<div style={{ margin: '8px 0' }}>
 				<button style={{ padding: '2px 12px' }}
@@ -118,9 +141,9 @@ export default function PlotExportView({ escape }: { escape: () => void }) {
 			</div>
 		</div>
 		<div ref={container} style={{ width: settings.width + 6, marginLeft: 8, cursor: 'pointer' }}>
-			{settings.plots.map((plot) => {
-				const Plot = trivialPlots[plot.type];
-				return <div key={JSON.stringify(plot)} style={{ height: plot.height + 2 }}>
+			{settings.plots.map(({ id, type, height }) => {
+				const Plot = trivialPlots[type];
+				return <div key={id} style={{ height: height + 2, position: 'relative' }}>
 					<Plot {...params}/>
 				</div>; })}
 		</div>
