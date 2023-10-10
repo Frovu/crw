654ddecc3fe4616b6a3ae821ebc4647fa0232653
Frovu <sfrovis@gmail.com>
2023-10-10 23:57:12 +0300

    i was thinking

--- client/src/events/Columns.tsx
+++ client/src/events/Columns.tsx
@@ -6,7 +6,6 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 import { immer } from 'zustand/middleware/immer';
 
-const G_TYPES = ['interval_value', 'local_value', 'combine_columns', 'clone_column'] as const;
 const EXTREMUM_OP = ['min', 'max', 'abs_min', 'abs_max'] as const;
 const G_COMBINE_OP = ['diff', 'abs_diff'] as const;
 const G_VALUE_OP = ['time_offset', 'time_offset_%', ...EXTREMUM_OP, 'mean', 'median', 'range', 'coverage'] as const;
@@ -24,36 +23,53 @@ type ReferencePoint = {
 	event: string,
 	end?: boolean,
 };
-type GenericColumnParams<T extends typeof G_TYPES[number]> = {
-	type: T,
-	column: T extends 'clone_columns'|'combine_columns' ? string : never,
-	clone_entity_shift: T extends 'clone_columns' ? number : never,
-	other_column: T extends 'combine_columns' ? string : never,
-	combine: T extends 'combine_columns' ? typeof G_COMBINE_OP[number] : never,
-	reference: T extends 'interval_value'|'local_value' ? ReferencePoint : never,
-	boundary: T extends 'interval_value' ? ReferencePoint : undefined,
-	operation: T extends 'interval_value'|'local_value' ? typeof G_VALUE_OP[number] : never,
-	series: T extends 'interval_value'|'local_value' ? string : never
+type GenericParamsClone = {
+	operation: 'clone_column',
+	column: string,
+	entity_offset: number,
 };
-type AnyGenericParams = GenericColumnParams<typeof G_TYPES[number]>;
+type GenericParamsCombine = {
+	operation: typeof G_COMBINE_OP[number],
+	column: string,
+	other_column: string,
+};
+type GenericParamsValue = {
+	operation: typeof G_VALUE_OP[number],
+	reference: ReferencePoint,
+	boundary: ReferencePoint,
+	series: string
+};
+type GenericParams = GenericParamsClone | GenericParamsCombine | GenericParamsValue;
+type GenericParamsOptions = { operation: typeof G_ALL_OPS[number] } & Omit<GenericParamsClone, 'operation'>
+& Omit<GenericParamsCombine, 'operation'> & Omit<GenericParamsValue, 'operation'>;
+
 export type GenericColumn = {
 	id: number,
-	params: AnyGenericParams
+	params: GenericParams
 };
 
 type GenericsState = {
 	id: number | null,
-	inputState: Partial<AnyGenericParams>,
+	inputState: Partial<GenericParamsOptions>,
 	nicknames: { [gid: string]: string },
 	setGeneric: (g: GenericColumn) => void,
-	set: <K extends keyof AnyGenericParams>(k: K, val?: AnyGenericParams[K]) => void,
+	set: <K extends keyof GenericParamsOptions>(k: K, val?: GenericParamsOptions[K]) => void,
 };
 const useGenericsState = create<GenericsState>()(persist(immer(set => ({
 	id: null,
 	inputState: {},
 	nicknames: {},
 	setGeneric: g => set(state => { state.id = g.id; state.inputState = g.params; }),
-	set: (k, val) => set(state => { state.inputState[k] = val; }),
+	set: (k, val) => set((state) => {
+		const inp = state.inputState;
+		if (k === 'operation') {
+			state.inputState = { [k]: val };
+			if (val === 'clone_column')
+				state.inputState.entity_offset = 0;
+			return;
+		}
+		inp[k] = val;
+	}),
 })), {
 	name: 'feidGenericColumns',
 	partialize: ({ nicknames }) => ({ nicknames })
@@ -65,13 +81,11 @@ export default function ColumnsSelector() {
 	const [action, setAction] = useState(true);
 	const [open, setOpen] = useState(false);
 	const { inputState, id: gid, setGeneric, set } = useGenericsState();
-	const { type: gtype, column, clone_entity_shift, other_column, combine,
-		reference } = inputState;
+	const { operation } = inputState as GenericParams;
 
 	const tables = allTables.filter(t => columns.find(c => c.table === t && c.name === 'time'));
-	const isClone = gtype === 'clone_column', isDiff = gtype?.startsWith('diff'), isTime = gtype?.startsWith('time');
-	const isSeriesColumn = isClone || isDiff;
-	const columnOpts = !isSeriesColumn ? null : columns
+	const isClone = operation === 'clone_column', isCombine = G_COMBINE_OP.includes(operation as any);
+	const columnOpts = (!isClone && !isCombine) ? null : columns
 		.filter(c => !c.hidden).map(({ id, fullName }) => [id, fullName]);
 	const entShort = (ent: string) => prettyTable(ent).replace(/([A-Z])[a-z ]+/g, '$1');
 
@@ -80,13 +94,17 @@ export default function ColumnsSelector() {
 	const check = (id: string, val: boolean) =>
 		setColumns(cols => val ? cols.concat(id) : cols.filter(c => c !== id));
 	
-	const Select = ({ txt, k, opts, required }: { txt: string, k: keyof typeof inputState, opts: string[][], required?: boolean }) =>
-		<div>{txt}:<select className='Borderless' style={!opts.find(o => o[0] === inputState[k]) ? { color: color('text-dark') } : {}} 
-			value={inputState[k] || 'null'}
-			onChange={e => set(k, e.target.value === 'null' ? undefined : e.target.value)}>
-			{!required && <option value='null'>{k === 'poi' ? 'default' : '-- None --'}</option>}
+	const Select = <T extends GenericParams>({ txt, k, opts, required }:
+	{ txt: string, k: T extends T ? keyof T : never, opts: string[][], required?: boolean }) =>
+		<div>{txt}:<select className='Borderless' style={!opts.find(o => o[0] === (inputState as T)[k]) ? { color: color('text-dark') } : {}} 
+			value={(inputState as T)[k] as any || 'null'}
+			onChange={e => set(k, e.target.value === 'null' ? undefined : e.target.value as any)}>
+			{!required && <option value='null'>-- None --</option>}
 			{opts.map(([val, pretty]) => <option key={val} value={val}>{pretty}</option>)}
 		</select></div>;
+	const RefInput = ({ which }: { which:'boundary'|'operation' }) => {
+		return <select></select>
+	}
 	return !open ? null : <>
 		<div className='PopupBackground' onClick={() => setOpen(false)}/>
 		<div className='Popup ColumnsSelector'>
@@ -108,20 +126,15 @@ export default function ColumnsSelector() {
 			</Fragment>)}
 			<div className='GenericsControls' style={{ }}>
 				<b>Create custom column</b>
-				<Select txt='Type' k='type' opts={TYPE_OPTIONS.map(t => [t, t])}/>
-				{gtype && !isTime && !isSeriesColumn &&
-					<Select txt='Series' k='series' opts={Object.entries(seriesOpts)}/>}
-				{isSeriesColumn && <Select txt='Column' k='series' opts={columnOpts!}/>}
-				{(series || isTime) && gtype && !isClone && <Select txt='POI' k='poi' opts={[['extremum', '<Extremum>']].concat(
-					tables.flatMap(tbl => [[tbl, prettyTable(tbl).slice(0, -1)],
-						...(columns.find(c => c.name === 'duration' && c.table === tbl) ?
-							[['end_'+tbl, entShort(tbl)+' End']] : [])]))}/>}
-				{poi === 'extremum' && <Select txt='Extrem' k='poi' opts={EXTREMUM_OP.map(e => [e, e])}/>}
-				{poi === 'extremum' && <Select txt='of' k='poi' opts={Object.entries(seriesOpts)}/>}
-				{gtype && isClone && <Select txt='Column' k='poi' opts={columnOpts!}/>}
-				{poi && !isDiff && gtype && <label>Offset, {!isTime && !isSeriesColumn ? 'hours' : 'events'}:
+				<Select txt='Type' k='operation' opts={G_ALL_OPS.map(t => [t, t])}/>
+				{(isClone || isCombine) && <Select txt='Column' k='column' opts={columnOpts!}/>}
+				{isCombine && <Select txt='Column' k='other_column' opts={columnOpts!}/>}
+				{isClone && <label>Offset, events:
 					<input style={{ width: 48, margin: '0 4px' }} type='number' step={1}
-						value={shift} onChange={e => set('shift', e.target.valueAsNumber)}/></label>}
+						value={inputState.entity_offset} onChange={e => set('entity_offset', 2)}/></label>}
+				{G_VALUE_OP.includes(operation as any) && <>
+					<Select txt='Series' k='series' opts={Object.entries(seriesOpts)}/>
+				</>}
 			</div>
 			<div className='CloseButton' style={{ position: 'absolute', top: 2, right: 4 }} onClick={() => setOpen(false)}/>
 		</div>
