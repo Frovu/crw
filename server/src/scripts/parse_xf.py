400c0bfcc8551f14859e67f30c533669b0dd7e6a
Frovu <sfrovis@gmail.com>
2024-04-02 15:39:08 +0300

    parse xfs based on offset

--- server/src/scripts/parse_xf.py
+++ server/src/scripts/parse_xf.py
@@ -6,13 +6,13 @@ from database import pool
 PATH = 'tmp/XF_table.txt'
 
 COLUMNS = {
-	'magnitude': None,
-	'gle': 'GLE',
-	'psi': 'psi',
-	'p10': 'p>10',
-	'p60': 'p>60',
-	'p100': 'p>100',
-	'dt_p10': 'dtp10'
+	'magnitude': (26, 39), # !
+	'gle': (145, 152),
+	'psi': (106, 108),
+	'p10': (109, 117),
+	'p60': (117, 126),
+	'p100': (126, 135),
+	'dt_p10': (135, 139)
 }
 
 def parse_xf():
@@ -20,19 +20,21 @@ def parse_xf():
 	with open(PATH, encoding='cp1251') as f:
 		for line in f:
 			if 'Date,' in line:
-				columns = [b for a in line.split(',') for b in (['XI', 'XMp'] if a.strip() == 'XIMp' else a.strip().split())]
 				break
 
-		index = [columns.index(c) for c in COLUMNS.values() if c]
+		slices = [slice(*c) for c in COLUMNS.values()]
 		for line in f:
-			if not line: continue
+			if not line:
+				continue
 			y, m, d = line[:10].split('.')
 			h, u, s = line[11:19].split(':')
-			xm = float(line[26:39]) * 1e6
-			split = line[90:].split()
 			time = datetime(*[int(a) for a in [y, m, d, h, u, s]], tzinfo=timezone.utc)
-			values = [float(split[i-12]) for i in index]
-			data.append([xm, *values, time])
+			values = [float(line[s].strip()) if line[s].strip() else None for s in slices]
+			print(values[2])
+			# xm !!!
+			values[0] *= 1e6
+
+			data.append([*values, time])
 
 	q = 'UPDATE events.solar_flares SET ' + ','.join([c + ' = %s' for c in [*COLUMNS.keys()]]) + ' WHERE time = %s'
 	with pool.connection() as conn:
