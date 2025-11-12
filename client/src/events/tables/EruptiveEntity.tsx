94d6e5a62ed6658cdbf3cfb0098faed742d86bf3
Semyon Belov <sfrovis@gmail.com>
2025-11-12 18:48:25 +0300

    refactor: dry eruptiveentitytable

---
+++ client/src/events/tables/EruptiveEntity.tsx
@@ -0,0 +1,130 @@
+import { openWindow } from '../../layout';
+import { valueToString } from '../core/eventsSettings';
+import { timeInMargin } from '../../util';
+import { EruptiveEntityMenu, EruptiveEntityTable } from './EruptiveEntityCore';
+import type { MouseEvent } from 'react';
+
+function CMEMenu() {
+	const openEnlil = (e: MouseEvent) => {
+		const params = { type: 'Sun View', mode: 'WSA-ENLIL' } as any;
+		openWindow({ x: e.clientX - 200, y: e.clientY - 200, w: 800, h: 400, params, unique: 'enlil-view' });
+	};
+
+	return (
+		<>
+			<EruptiveEntityMenu entity="cme" />
+			<button className="TextButton" onClick={openEnlil}>
+				Open ENLIL view
+			</button>
+			<div className="separator" />
+			<a className="Row" href="https://cdaw.gsfc.nasa.gov/CME_list/" target="_blank" rel="noreferrer">
+				LASCO Catalogue
+			</a>
+		</>
+	);
+}
+
+function FLRMenu() {
+	return <EruptiveEntityMenu entity="flare" />;
+}
+
+function ICMEMenu() {
+	return (
+		<>
+			<EruptiveEntityMenu entity="icme" />
+			<div className="separator" />
+			<a
+				className="Row"
+				href="https://izw1.caltech.edu/ACE/ASC/DATA/level3/icmetable2.htm"
+				target="_blank"
+				rel="noreferrer"
+			>
+				R&C Catalogue
+			</a>
+		</>
+	);
+}
+
+function CMEPanel() {
+	return (
+		<EruptiveEntityTable
+			entity="cme"
+			rowColorCallback={({ event: cme, erupt, feid }) => {
+				const linkedEvents = cme.linked_events?.split(',');
+				const linked = linkedEvents?.find(
+					(lnk) =>
+						(lnk.includes('GST') || lnk.includes('IPS')) &&
+						timeInMargin(feid.time, new Date(lnk.slice(0, 19) + 'Z'), 8 * 36e5)
+				);
+
+				if (linked) return 'text-orange';
+
+				const cmeTime = erupt?.cme_time ?? feid.cme_time;
+				const nearCme = cmeTime && timeInMargin(cme.time, cmeTime, 6e5);
+
+				return nearCme ? 'text-orange' : null;
+			}}
+			cellContent={valueToString}
+		/>
+	);
+}
+
+function FLRPanel() {
+	return (
+		<EruptiveEntityTable
+			entity="flare"
+			rowColorCallback={({ event: flare, erupt, feid }) => {
+				const flrTime = erupt?.flr_start ?? feid.flr_time;
+				const cmeTime = erupt?.cme_time ?? feid.cme_time;
+
+				if (flrTime && timeInMargin(flare.start_time, flrTime, 6e5)) return 'text-orange';
+
+				const linkedEvents = flare.linked_events?.split(',');
+				const linkedToCme =
+					cmeTime &&
+					linkedEvents?.find(
+						(lnk) => lnk.includes('CME') && timeInMargin(cmeTime, new Date(lnk.slice(0, 19) + 'Z'), 6e5)
+					);
+
+				return linkedToCme ? 'text-orange' : null;
+			}}
+			cellContent={(val, column) => {
+				const str = valueToString(val);
+				return ['peak', 'end'].includes(column.name) ? str.split(' ')[1] : str;
+			}}
+		/>
+	);
+}
+
+function ICMEPanel() {
+	return (
+		<EruptiveEntityTable
+			entity="icme"
+			rowColorCallback={({ event: icme, erupt, feid }) => {
+				const startClose = timeInMargin(icme.time, feid.time, 4 * 36e5);
+				const mcClose = feid.mc_time && timeInMargin(icme.body_start, feid.mc_time, 4 * 36e5);
+
+				return startClose || mcClose ? 'text-orange' : null;
+			}}
+			cellContent={valueToString}
+		/>
+	);
+}
+
+export const FlaresTable = {
+	name: 'Flares Table',
+	Panel: FLRPanel,
+	Menu: FLRMenu,
+};
+
+export const CMETable = {
+	name: 'CME Table',
+	Panel: CMEPanel,
+	Menu: CMEMenu,
+};
+
+export const ICMETable = {
+	name: 'ICME Table',
+	Panel: ICMEPanel,
+	Menu: ICMEMenu,
+};
---
+++ client/src/events/tables/EruptiveEntityCore.tsx
@@ -0,0 +1,126 @@
+import { useContext } from 'react';
+import { sourceLabels, sourceLinks, type Column, type Tables } from '../../api';
+import { useContextMenu } from '../../app';
+import { LayoutContext } from '../../layout';
+import { cn } from '../../util';
+import { type TableValue, useTable } from '../core/editableTables';
+import { equalValues } from '../core/eventsSettings';
+import { useFeidCursor, useSelectedSource, useEntityCursor } from '../core/eventsState';
+import { useSolarPlot } from '../core/plot';
+import { useCompoundTable } from '../core/query';
+import {
+	type EruptTable,
+	type EruptiveEvent,
+	getSourceLink,
+	linkEruptiveSourceEvent,
+	unlinkEruptiveSourceEvent,
+	compoundTables,
+} from '../core/sourceActions';
+import { TableWithCursor, DefaultRow, DefaultCell } from './Table';
+
+export function EruptiveEntityMenu<T extends EruptTable>({ entity }: { entity: T }) {
+	const detail = useContextMenu((state) => state.menu?.detail) as { [key in T]: EruptiveEvent<T> } | undefined;
+	const { id } = useFeidCursor();
+	const event = detail?.[entity];
+	const erupt = useSelectedSource('sources_erupt');
+
+	if (!event) return null;
+
+	const src = event.src;
+	const link = getSourceLink(entity, src);
+	const isLinked = equalValues(event[link.id], erupt?.[link.link]);
+
+	return (
+		<>
+			<button
+				className={cn('TextButton', erupt?.[link.link] ? 'text-text-dark' : 'text-text')}
+				onClick={() => id && linkEruptiveSourceEvent(entity, event, id)}
+			>
+				Link {src} {entity.toUpperCase()}
+			</button>
+			{isLinked && (
+				<button className="TextButton" onClick={() => unlinkEruptiveSourceEvent(entity, event)}>
+					Unlink {src} {entity.toUpperCase()}
+				</button>
+			)}
+		</>
+	);
+}
+
+type PanelProps<T extends EruptTable> = {
+	entity: T;
+	cellContent: (val: TableValue, col: Column) => string;
+	rowColorCallback: (arg: {
+		erupt: Tables['sources_erupt'] | null;
+		event: EruptiveEvent<T>;
+		feid: Tables['feid'];
+	}) => string | null;
+};
+export function EruptiveEntityTable<T extends EruptTable>({ entity, rowColorCallback, cellContent }: PanelProps<T>) {
+	const { id: nodeId, size } = useContext(LayoutContext)!;
+	const cursor = useEntityCursor(entity);
+	const erupt = useSelectedSource('sources_erupt');
+	const eruptions = useTable('sources_erupt');
+	const table = useCompoundTable(entity);
+	const solar = useSolarPlot();
+
+	const { id: feidId, row: feid } = useFeidCursor();
+
+	if (!table) return <div className="Center">LOADING..</div>;
+
+	const { data, columns, entry } = table;
+	const focusTime = solar.focusTime.getTime();
+	const timeIdx = table.index[(entity === 'flare' ? 'start_time' : 'time') as keyof typeof table.index];
+	const focusIdx = focusTime == null ? data.length : data.findIndex((r) => (r[timeIdx] as Date)?.getTime() > focusTime);
+	const linked =
+		erupt && Object.fromEntries(compoundTables.cme.map((ent) => [sourceLabels[ent], erupt[sourceLinks[ent][0]]]));
+
+	return (
+		<TableWithCursor
+			{...{
+				entity,
+				data,
+				columns,
+				size,
+				focusIdx,
+				onKeydown: (e) => {
+					if (cursor && erupt && e.key === '-') return unlinkEruptiveSourceEvent(entity, entry(data[cursor.row]));
+					if (cursor && ['+', '='].includes(e.key))
+						return feidId && linkEruptiveSourceEvent(entity, entry(data[cursor.row]), feidId);
+				},
+				row: (row, idx, onClick, padRow) => {
+					const event = entry(row);
+					const link = getSourceLink(entity, event.src);
+					const isLinked = equalValues(event[link.id], linked?.[event.src]);
+
+					const className = (() => {
+						if (isLinked) return erupt?.cme_source === event.src ? 'text-cyan font-bold' : 'text-cyan/90';
+						if (linked?.[event.src])
+							// Other similar event linked to current erupt
+							return 'text-text-dark';
+						const linkIdx = eruptions.index[link.link];
+						const linkedTo = eruptions.data?.find((eru) => equalValues(event[link.id], eru[linkIdx]));
+						if (linkedTo)
+							// This event is already linked to some eruption // FIXME: this is probably slow
+							return 'text-text-dark';
+						return rowColorCallback({ erupt, feid, event }) ?? 'text-text';
+					})();
+
+					return (
+						<DefaultRow
+							key={event.src + event[link.id] + row[2]}
+							{...{ row, idx, columns, cursor, className, padRow }}
+							onClick={(e, cidx) => {
+								if (cidx === 0 && feidId !== null) return linkEruptiveSourceEvent(entity, entry(row), feidId);
+								onClick(idx, cidx);
+							}}
+							contextMenuData={() => ({ nodeId, [entity]: event })}
+						>
+							{({ column, cidx }) => <DefaultCell column={column}>{cellContent(row[cidx], column)}</DefaultCell>}
+						</DefaultRow>
+					);
+				},
+			}}
+		/>
+	);
+}
