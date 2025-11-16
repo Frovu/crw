43fa54a74051d6b96d3fd1dc382e89eab7dcd970
Semyon Belov <sfrovis@gmail.com>
2025-11-16 21:16:58 +0300

    table input

--- client/src/events/tables/TableInput.tsx
+++ client/src/events/tables/TableInput.tsx
@@ -1,82 +1,68 @@
-import { useState, useMemo, type ChangeEvent } from 'react';
+import { useState, type ChangeEvent } from 'react';
 import type { Column } from '../../api';
-import { type EditableTable, makeChange } from '../core/editableTables';
+import { type TableValue } from '../core/editableTables';
 import { useEventsState } from '../core/eventsState';
-import { parseColumnValue, isValidColumnValue } from '../core/util';
-import { color } from '../../app';
+import { parseColumnValue, isValidColumnValue, valueToString } from '../core/util';
+import { cn } from '../../util';
 
-export function CellInput({
-	id,
+export function TableInput({
 	column,
 	value,
-	table,
 	options,
-	change,
+	onChange,
 }: {
-	id: number;
 	column: Column;
-	value: string;
-	table: EditableTable;
+	value: TableValue;
 	options?: string[];
-	change?: (val: any) => boolean;
+	onChange: (val: TableValue) => void;
 }) {
 	const [invalid, setInvalid] = useState(false);
 	const escapeCursor = useEventsState((state) => state.escapeCursor);
 
-	return useMemo(() => {
-		const doChange = (v: any) => (change ? change(v) : makeChange(table, { id, column: column.sql_name, value: v }));
+	const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>, save: boolean = false) => {
+		const str = e.target.value.trim();
+		const val = str === '' ? null : str === 'auto' ? str : parseColumnValue(str, column);
+		const isValid = ['auto', null].includes(val as any) || isValidColumnValue(val, column);
+		const isOk = isValid && (!save || onChange(val));
+		setInvalid(!isOk);
+	};
 
-		const onChange = (e: ChangeEvent<HTMLInputElement | HTMLSelectElement>, save: boolean = false) => {
-			const str = e.target.value.trim();
-			const val = str === '' ? null : str === 'auto' ? str : parseColumnValue(str, column);
-			const isValid = ['auto', null].includes(val as any) || isValidColumnValue(val, column);
-			const isOk = isValid && (!save || doChange(val));
-			setInvalid(!isOk);
-		};
+	const className = cn('w-full border-0 bg-active/15 text-center', invalid && 'text-red bg-red/15');
 
-		const inpStype = {
-			width: '100%',
-			borderWidth: 0,
-			padding: 0,
-			backgroundColor: color('bg'),
-			boxShadow: column.dtype !== 'enum' ? ' 0 0 16px 4px ' + (invalid ? color('red') : color('active')) : 'unest',
-		};
-
-		return (
-			<>
-				{column.dtype === 'enum' && (
-					<select
-						autoFocus
-						style={inpStype!}
-						value={value}
-						onChange={(e) => {
-							onChange(e, true);
-							escapeCursor();
-						}}
-					>
-						{column.type === 'static' && !column.not_null && <option value="">&lt;null&gt;</option>}
-						{column.type === 'static' &&
-							(options ?? column.enum)?.map((val) => (
-								<option key={val} value={val}>
-									{val}
-								</option>
-							))}
-					</select>
-				)}
-				{column.dtype !== 'enum' && (
-					<input
-						type="text"
-						autoFocus
-						style={inpStype!}
-						defaultValue={value}
-						onChange={onChange}
-						onBlur={(e) => {
-							e.target.value !== value && onChange(e, true);
-							escapeCursor();
-						}}
-					/>
-				)}
-			</>
-		);
-	}, [column.type, id, JSON.stringify(options), invalid, table, value]); // eslint-disable-line
+	return (
+		<>
+			{column.dtype === 'enum' && (
+				<select
+					autoFocus
+					className={className}
+					value={valueToString(value)}
+					onChange={(e) => {
+						handleChange(e, true);
+						escapeCursor();
+					}}
+				>
+					{column.type === 'static' && !column.not_null && <option value="">&lt;null&gt;</option>}
+					{column.type === 'static' &&
+						(options ?? column.enum)?.map((val) => (
+							<option key={val} value={val}>
+								{val}
+							</option>
+						))}
+				</select>
+			)}
+			{column.dtype !== 'enum' && (
+				<input
+					type="text"
+					autoFocus
+					className={className}
+					defaultValue={valueToString(value)}
+					onChange={handleChange}
+					onBlur={(e) => {
+						if (e.target.value !== valueToString(value)) handleChange(e, true);
+						escapeCursor();
+					}}
+				/>
+			)}
+		</>
+	);
 }
