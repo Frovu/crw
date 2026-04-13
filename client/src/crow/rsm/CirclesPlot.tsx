7dd25c57ee9f9cf03b0dd922551d0bb15b993954
Semyon Belov <sfrovis@gmail.com>
2026-04-13 23:44:33 +0300

    rsm multibased plot

---
+++ client/src/crow/rsm/CirclesPlot.tsx
@@ -0,0 +1,139 @@
+import { useMemo, useRef, useState } from 'react';
+import { apiGet, useSize } from '../../util';
+import { circlePaths, linePaths } from '../../plots/plotPaths';
+import {
+	axisDefaults,
+	color,
+	customTimeSplits,
+	drawMagneticClouds,
+	drawOnsets,
+	scaled,
+	usePlotOverlay,
+} from '../../plots/plotUtil';
+import { applyTextTransform } from '../../plots/basicPlot';
+import { useQuery } from '@tanstack/react-query';
+import uPlot from 'uplot';
+
+import { ExportableUplot } from '../../events/export/ExportableUplot';
+import type { ContextMenuProps } from '../../app/layout';
+import { usePlot } from '../../events/core/plot';
+import type { RSMPlotResponse } from '../../api';
+import { drawCirclesLegend, NEG_S, POS_S, renderCirclesData } from './circlesPlot';
+
+const defaultParams = {
+	variationShift: 0,
+	sizeShift: 0,
+	linearSize: false,
+};
+
+export type CirclesPlotParams = typeof defaultParams;
+
+function Menu({ params, Checkbox, setParams }: ContextMenuProps<CirclesPlotParams>) {
+	return <></>;
+}
+
+function Panel() {
+	const params = usePlot<CirclesPlotParams>();
+	const { interval, onsets, variationShift } = params;
+
+	const overlayHandle = usePlotOverlay((u, { width }) => ({
+		x: (u.bbox.left + u.bbox.width - scaled(width)) / scaled(1) + 6,
+		y: u.bbox.top / scaled(1),
+	}));
+
+	const query = useQuery({
+		queryKey: ['rsm', interval, onsets],
+		queryFn: () =>
+			apiGet<RSMPlotResponse>('crow/rsm/historical', {
+				from: interval[0].getTime() / 1e3,
+				to: interval[1].getTime() / 1e3,
+				events: onsets?.map((ons) => ons.time.getTime() / 1e3),
+			}),
+	});
+
+	const [uplot, setUplot] = useState<uPlot>();
+	const plotData = useMemo(() => {
+		if (!query.data) return null;
+		return renderCirclesData(query.data, variationShift);
+	}, [query.data, variationShift]);
+
+	const options: (() => Omit<uPlot.Options, 'width' | 'height'>) | null = useMemo(() => {
+		return () => ({
+			padding: [scaled(8), scaled(48), 0, 0],
+			mode: 2,
+			legend: { show: false },
+			cursor: {
+				show: false,
+			},
+			hooks: {
+				draw: [drawMagneticClouds(params), drawOnsets(params), drawCirclesLegend({ params, overlayHandle, plotData })],
+				ready: [overlayHandle.onReady],
+			},
+			axes: [
+				{
+					...axisDefaults(params.showGrid),
+					...customTimeSplits(params),
+				},
+				{
+					...axisDefaults(params.showGrid),
+					ticks: { ...axisDefaults(params.showGrid).ticks, size: 4 },
+					scale: 'y',
+					label: applyTextTransform('effective longitude, deg'),
+					values: (u, vals) => vals.map((v) => v.toFixed(0)),
+					space: scaled(32),
+					gap: scaled(2),
+					incrs: [30, 45, 60, 90, 180, 360],
+				},
+				{
+					scale: 'idx',
+					show: false,
+				},
+			],
+			scales: {
+				x: {
+					time: false,
+					range: (u, min, max) => [min, max],
+				},
+				y: {
+					range: [-5, 365],
+				},
+			},
+			series: [
+				{ facets: [{ scale: 'x', auto: true }] },
+				{
+					label: '+',
+					facets: [
+						{ scale: 'x', auto: true },
+						{ scale: 'y', auto: true },
+					],
+					stroke: color('cyan'),
+					fill: color('cyan2'),
+					paths: circlePaths(null, POS_S, params),
+				},
+				{
+					label: '-',
+					facets: [
+						{ scale: 'x', auto: true },
+						{ scale: 'y', auto: true },
+					],
+					stroke: color('magenta'),
+					fill: color('magenta2'),
+					paths: circlePaths(null, NEG_S, params),
+				},
+			],
+		});
+	}, [params, plotData]);
+
+	if (query.isError) throw query.error;
+	if (query.isLoading || !plotData) return <div className="center">LOADING...</div>;
+
+	return <ExportableUplot {...{ options, data: plotData as any, onCreate: setUplot }} />;
+}
+
+export const RSMPlot = {
+	name: 'Ring of Stations 2',
+	Panel,
+	Menu,
+	defaultParams,
+	isPlot: true,
+};
---
+++ server/src/crow/rsm/historical.py
@@ -0,0 +1,31 @@
+import warnings
+import numpy as np
+from crow.rsm.core import RSMPlotResponse, fetch_counts, filter_variations
+
+HOUR = 3600
+BASE_LEN = 24
+
+def fetch_variations(t_from: int, t_to: int, event_starts: list[int]):
+	data, stations = fetch_counts(t_from, t_to)
+	time, data = data[:,0].astype(int), data[:,1:]
+	
+	t_from = time[0]
+
+	result = np.full_like(data, np.nan)
+	event_idxes = np.array(event_starts) // HOUR - t_from // HOUR
+
+	with warnings.catch_warnings():
+		warnings.filterwarnings(action='ignore', message='Mean of empty slice')
+
+		cur = 0
+		for i, event_idx in enumerate(event_idxes):
+			base_start = max(0, event_idx - BASE_LEN )
+			base_data = data[base_start : base_start + BASE_LEN + 2]
+			base = np.nanmean(base_data, axis=0)
+			part_end = event_idxes[i + 1] if i + 1 < len(event_idxes) else len(result)
+			result[cur:part_end] = data[cur:part_end] / base * 100 - 100
+			cur = part_end
+
+	filter_variations(result)
+	result = np.where(~np.isfinite(result.T), None, np.round(result.T, 2)).tolist() # type: ignore
+	return RSMPlotResponse(time.tolist(), result, stations).as_dict()
