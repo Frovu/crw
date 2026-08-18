3de189d955937e9cea74403a3bd9c1dd3c372aac
Semyon Belov <sfrovis@gmail.com>
2026-08-18 18:46:26 +0300

    toggle Vsw and Bt

--- client/src/plots/time/IMF.tsx
+++ client/src/plots/time/IMF.tsx
@@ -9,21 +9,26 @@ import type { CustomSeries } from '../common/types';
 const defaultParams = {
 	showBz: true,
 	showBxBy: false,
+	showSpeed: true,
+	showBt: true,
 };
 
 export type IMFParams = typeof defaultParams;
 
 function Menu({ Checkbox }: ContextMenuProps<IMFParams>) {
 	return (
-		<>
+		<div className="flex flex-wrap max-w-60 gap-x-4">
+			<Checkbox label="Show Vsw" k="showSpeed" />
+			<Checkbox label="Show |B|" k="showBt" />
 			<Checkbox label="Show Bx, By" k="showBxBy" />
 			<Checkbox label="Show Bz" k="showBz" />
-		</>
+		</div>
 	);
 }
 
 function Panel() {
 	const params = usePlot<IMFParams>();
+	const anyB = params.showBt || params.showBxBy || params.showBz;
 
 	return (
 		<BasicPlot
@@ -33,21 +38,24 @@ function Panel() {
 				params,
 				axes: () => [
 					{
+						show: params.showSpeed,
 						label: 'Vsw',
-						position: [1 / 2, 1],
+						position: [anyB ? 1 / 2 : 0, 1],
 						fullLabel: 'Vsw, km/s',
 						showGrid: false,
 						side: 1,
 					},
 					{
+						show: anyB,
 						label: 'IMF',
-						position: [0, 3 / 5],
+						position: [0, params.showSpeed ? 3 / 5 : 1],
 						fullLabel: `IMF(|B|${params.showBxBy ? ',Bx,By' : ''}${params.showBz ? ',Bz' : ''}), nT`,
 						whole: true,
 					},
 				],
 				series: () => [
 					{
+						show: params.showSpeed,
 						label: 'Vsw',
 						legend: 'Vsw',
 						scale: 'Vsw',
@@ -56,6 +64,7 @@ function Panel() {
 						marker: 'diamond',
 					},
 					{
+						show: params.showBt,
 						label: '|B|',
 						legend: 'IMF |B|',
 						scale: 'IMF',
